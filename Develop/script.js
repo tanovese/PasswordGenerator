9979dd2bb2d08b0e641916d9b653bb3deeaf1713
tanovese <82059748+tanovese@users.noreply.github.com>
2023-04-30 22:18:40 -0400

    Fixed runPrompts function

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,47 @@
 // Assignment code here
 
-var passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters");
+function runPrompts() {
+  passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters");
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+      alert("Input is invalid");
+      return false;
+    } else {
+      true;
+    }
 
-var lowercasePrompt= (window.confirm("Would you like to use lowercase?"));
+  lowercasePrompt= (window.confirm("Would you like to use lowercase?"));
 
-var uppercasePrompt= (window.confirm("Would you like to use uppercase?"));
+  uppercasePrompt= (window.confirm("Would you like to use uppercase?"));
 
-var numericPrompt= (window.confirm("Would you like to use numeric characters?"));
+  numericPrompt= (window.confirm("Would you like to use numeric characters?"));
 
-var specialPrompt = (window.confirm("Would you like to use special characters?"));
+  specialPrompt = (window.confirm("Would you like to use special characters?"));
 
+}
 var generateBtn = document.querySelector("#generate");
 
-// if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-//   alert("Input is invalid");
-//   return false;
-// } else {
-
-generateBtn.addEventListener("click", windowPrompts);
+generateBtn.addEventListener("click", runPrompts);
 
 // random character strings //
-  
-  let numerics =String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 
+  let randomNumeric = String.fromCharCode(Math.floor(Math.random() *10) + 48);
+  
   let randomLower = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 
   let randomUpper = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 
-  let specials= String.fromCharCode(Math.floor(Math.random() * 6) + 33);
+  let randomSpecial= String.fromCharCode(Math.floor(Math.random() * 6) + 33);
 
-  console.log(numerics);
+  console.log();
+
 
 // have to define generate password //
 
 function generatePassword() { 
-  if (passwordLength === numerics) {
-    return true;
-  } if (lowercasePrompt === true) {
-    return true;
-  } if (uppercasePrompt === true) {
-    return true;
-  } if (specialPrompt === true) {
-    return true;
-  }
 }
 
 function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  if (generatePassword()=== true) {
   return password;
-  }
 }
