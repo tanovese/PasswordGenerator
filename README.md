904254a9ac6903fc780c7ed0a72d16c9c35a433d
tanovese <82059748+tanovese@users.noreply.github.com>
2023-05-03 18:11:15 -0400

    Added special characters if statement. Added Readme. Tested final code

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Description
+
+This application generates a random and secure password.
+
+## Installation
+
+N/A
+
+## Usage
+
+Click generate password on the webpage and you will be prompted to choose a password length, requiring at least 8 characters and no more than 128 characters. After choosing the length, you will be prompted to choose whether or not you would like to include lowercase characters, uppercase characters, numeric characters, and or special characters. These are simply yes or no questions and the user can choose yes by clicking "Ok" or no by clicking "Cancel". Once the user answers the necessary prompts, a random password is generated based on their answers and is displayed on the webpage. Right click on the screen and choose inspect to view the code.
+
+## Screenshot and Link to Application
+
+Screenshot: ![Screenshot of the Password Generator Webpage](./images/Screenshot%202023-05-03%20180638.png)
+
+Link: 
+
+## Credits
+
+Some of the code in this project was done with the help of Andres Long, a Rutgers BCS tutor.
+
+## License
+
+N/A
--- script.js
+++ script.js
@@ -34,17 +34,6 @@ function runPrompts() {
     chosenCharacters += "!@#$%&*?"
   }
 
-  // var num = 0;
-  // num = num + 1;
-  // num += 1;
-  // num++
-
-  //strings
-  // var str = "dog"
-  // var sentence = "dog" + " cat"
-  // sentence = sentence + str;
-  // sentence += str; 
-
   var passwordResult = ""
 
   for(i=0; i<passwordLength; i++){
@@ -56,37 +45,6 @@ function runPrompts() {
   var passwordText = document.querySelector("#password");
   passwordText.value = passwordResult;
 
-  // for(j=0; j < 20; j++) {
-  //   console.log("something")
-  // }
-
-
 }
 
 generateBtn.addEventListener("click", runPrompts);
-
-// random character strings //
-
-// let randomNumeric = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-
-// let randomLower = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-
-// let randomUpper = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-
-// let randomSpecial = String.fromCharCode(Math.floor(Math.random() * 6) + 33);
-
-
-// have to define generate password //
-
-// function generatePassword() {
-// }
-
-// function writePassword() {
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-
-// //   for (var i=0; i < //variabel here, i++) {
-// //   password= password + //variable here or function here?[Math.floor(Math.random() * same variable or funciton here?.length)];
-// // }
-// //   return password;
-// // }
